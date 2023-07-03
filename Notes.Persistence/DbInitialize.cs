eeb30876162e5cf2a785b875ea183a39171b9b1f
ZvalsoraY <yazamail@yandex.ru>
2023-07-03 20:45:20 +0300

    Add project files.

---
+++ Notes.Persistence/DbInitialize.cs
@@ -0,0 +1,17 @@
+﻿using Notes.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Persistence
+{
+    public class DbInitialize
+    {
+        public static void Initialize(NotesDbContext context) 
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
---
+++ Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Persistence.EntityTypeConfigurations;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notes.Persistence
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set;}
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options) { } 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConfigguration());
+            base.OnModelCreating(builder);
+        }
+
+        public Task<int> SaveChangeAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
